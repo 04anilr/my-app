6c5d85fdf648e215ac04d48540232dfcfae8570f
anilr <anilr5364@gmail.com>
2024-03-14 14:07:24 +0530

    create rendom list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+// App.js
+
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const fetchRandomUser = async () => {
+    try {
+      const randomNumber = Math.floor(Math.random() * 82) + 1;
+      const response = await fetch(`https://swapi.dev/api/people/${randomNumber}`);
+      const data = await response.json();
+      
+      setUsers([...users, data]);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const deleteUser = (index) => {
+    const updatedUsers = [...users];
+    updatedUsers.splice(index, 1);
+    setUsers(updatedUsers);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchRandomUser}>Add Record</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Height</th>
+            <th>Mass</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <td>{user.name}</td>
+              <td>{user.height}</td>
+              <td>{user.mass}</td>
+              <td><button onClick={() => deleteUser(index)}>Delete</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
 
 export default App;
+
+
